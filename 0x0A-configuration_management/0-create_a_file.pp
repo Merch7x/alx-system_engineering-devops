5adae538e6c1c0daf33063851fd06b01e4b5f19b
Merch7x <aquamaingi@gmail.com>
2024-02-23 16:02:06 +0300

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creates a file and adds content to it
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
+
