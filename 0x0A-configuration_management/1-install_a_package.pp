99d9969874fdea21620a74c317a1d46e34aa582a
Merch7x <aquamaingi@gmail.com>
2024-02-23 17:07:50 +0300

    Refactor solution

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
 # install flask from pip3
-package {'python3-pip':
-  ensure   => 'installed',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
-
-exec { 'install flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 freeze | /bin/grep Flask | /bin/grep 2.1.0',
-}
-
