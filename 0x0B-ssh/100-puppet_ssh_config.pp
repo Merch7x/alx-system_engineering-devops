dc86e64c9aec54c0c752ae49c9b3ba423b90960c
Merch7x <aquamaingi@gmail.com>
2024-02-25 17:55:48 +0300

    Update puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 file_line {'SSH-Private-Key':
   path               => '/etc/ssh/ssh_config',
   line               => '   IdentityFile ~/.ssh/school',
-  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$'
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
   replace            => true,
   append_on_no_match => true
 }
