5ca132b14bd11c273bf0ce61cf28618e8fa4ef7a
Merch7x <aquamaingi@gmail.com>
2024-04-09 22:28:34 +0300

    Trial

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,45 +5,57 @@ import requests
 from requests.exceptions import HTTPError
 
 
-client_id = "6YmHWBdlASK8HtHgAOwJ8A"
-client_secret = "NPiEFJ5bqOKcPOI7DfvT-AMPzU6Pug"
-user_agent = "MerchApi by FitInspection9287"
-username = "FitInspection9287"
-password = "ckent2535"
+# client_id = "6YmHWBdlASK8HtHgAOwJ8A"
+# client_secret = "NPiEFJ5bqOKcPOI7DfvT-AMPzU6Pug"
+# user_agent = "MerchApi by FitInspection9287"
+# username = "FitInspection9287"
+# password = "ckent2535"
 
-headers = {'User-Agent': 'MyApi/1'}
-
-
-def get_access_token(client_id, client_secret):
-    """Retrieve auth token"""
-    data = {
-        'grant_type': 'client_credentials',
-        'username': username,
-        'passowrd': password
-    }
-    auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-    url = 'https://www.reddit.com/api/v1/access_token'
-    response = requests.post(url, auth=auth, data=data, headers=headers)
-    token = response.json()['access_token']
-    return token
+# headers = {'User-Agent': 'MyApi/1'}
 
 
-headers['Authorization'] = "bearer {}".format(
-   get_access_token(
-    client_id=client_id, client_secret=client_secret))
+# def get_access_token(client_id, client_secret):
+#     """Retrieve auth token"""
+#     data = {
+#         'grant_type': 'client_credentials',
+#         'username': username,
+#         'passowrd': password
+#     }
+#     auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+#     url = 'https://www.reddit.com/api/v1/access_token'
+#     response = requests.post(url, auth=auth, data=data, headers=headers)
+#     token = response.json()['access_token']
+#     return token
 
+
+# headers['Authorization'] = "bearer {}".format(
+#    get_access_token(
+#     client_id=client_id, client_secret=client_secret))
+
+
+# def number_of_subscribers(subreddit):
+#     """Return subscriber count"""
+#     try:
+#         response = requests.get(
+#             f'https://oauth.reddit.com/r/{subreddit}/about', headers=headers,
+#             allow_redirects=False)
+#         response.raise_for_status()
+#     except HTTPError as http_err:
+#         return 0
+#     except Exception as err:
+#         return 0
+#     else:
+#         total_sub = response.json().get('data')['subscribers']
+#         return total_sub
 
 def number_of_subscribers(subreddit):
-    """Return subscriber count"""
-    try:
-        response = requests.get(
-            f'https://oauth.reddit.com/r/{subreddit}/about', headers=headers,
-            allow_redirects=False)
-        response.raise_for_status()
-    except HTTPError as http_err:
-        return 0
-    except Exception as err:
-        return 0
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
     else:
-        total_sub = response.json().get('data')['subscribers']
-        return total_sub
+        return 0
