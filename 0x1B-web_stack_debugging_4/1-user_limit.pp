49335a2471f5d4cbc2afaed2ae9ab8f5858a9eb1
Merch7x <aquamaingi@gmail.com>
2024-04-18 19:44:03 +0300

    modify script

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -6,6 +6,6 @@ exec { 'increase-hard-file-limit-for-holberton-user':
 }
 
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf'
   path    => '/usr/local/bin/:/bin/'
 }
